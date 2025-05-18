36b4f8755e402d8685faf034113cbf576fddb2b0
Reinaldo <reinaldo.rossetti@outlook.com>
2025-05-18 15:47:33 -0300

    novos exemplos

--- lib/selead.dart
+++ lib/selead.dart
@@ -1,49 +1,9 @@
-sealed class HttpState {}
+sealed class Veiculo {
+  final String marca;
+  final String modelo;
+  final int ano;
 
-class Loading extends HttpState {}
+  Veiculo(this.marca, this.modelo, this.ano);
 
-class Error extends HttpState {
-  final String message;
-  Error(this.message);
-}
-
-class Loaded extends HttpState {
-  final String data;
-  Loaded(this.data);
-}
-
-Future<HttpState> fetchData({bool succeed = true}) async {
-  // Simulate network delay
-  await Future.delayed(Duration(seconds: 2));
-
-  if (succeed) {
-    return Loaded('{"message": "Hello from server!"}');
-  } else {
-    return Error('Failed to fetch data.');
-  }
-}
-
-void handleState(HttpState state) {
-  switch (state) {
-    case Loading():
-      print('⏳ Loading...');
-      break;
-
-    case Loaded(:final data):
-      print('✅ Success! Response: $data');
-      break;
-
-    case Error(:final message):
-      print('❌ Error: $message');
-      break;
-  }
-}
-
-void main() async {
-  HttpState state = Loading();
-  handleState(state);
-
-  // Fetching data, change to false to simulate error
-  state = await fetchData(succeed: true);
-  handleState(state);
+  void detalhes();
 }
